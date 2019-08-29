c82d0446b00c8d346397d05017ab89317b002199
MaxShilo <macsim.shilovsky@yandex.ru>
2019-08-29 11:16:19 +0300

    Added export to exel

---
+++ PhonebookCore/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhonebookCore
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=PhonebookApp; Integrated Security=True;";
+        int PhoneBookID = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() != "" && textBox2.Text.Trim() != "" && textBox3.Text.Trim() != "" && textBox4.Text.Trim() != "")
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    SqlCommand sqlCmd = new SqlCommand("ContactAddOrEdit", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@PhoneBookID", PhoneBookID);
+                    sqlCmd.Parameters.AddWithValue("@Name", textBox1.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Surname", textBox2.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Patronymic", textBox3.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Phone", textBox4.Text.Trim());
+                    sqlCmd.ExecuteNonQuery();
+                    MessageBox.Show("Изменение прошло успешно");
+
+                    Clear();
+                    GridFill();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, заполните все поля");
+            }
+        }
+
+        void Clear()
+        {
+            textBox1.Text = textBox2.Text = textBox3.Text = textBox4.Text = textBox5.Text = "";
+            PhoneBookID = 0;
+            button1.Text = "Сохранено";
+            button2.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e) 
+        {
+             Clear();
+        }
+
+        void GridFill()
+        {
+            using (SqlConnection sqlConn = new SqlConnection(connectionString))
+            {
+                sqlConn.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("ContactViewAll", sqlConn);
+                sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable dbTbl = new DataTable();
+                sqlDa.Fill(dbTbl);
+                dataGridView1.DataSource = dbTbl;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GridFill();
+            button2.Enabled = false;
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow.Index != -1)
+            {
+                textBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                textBox2.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                textBox3.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+                textBox4.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+                PhoneBookID = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+
+                button1.Text = "Изменение";
+                button2.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlCommand sqlCmd = new SqlCommand("ContactDeleteByID", sqlCon);
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                sqlCmd.Parameters.AddWithValue("@PhoneBookID", PhoneBookID);
+                sqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Контакт удален");
+
+                Clear();
+                GridFill();
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlConn = new SqlConnection(connectionString))
+            {
+                sqlConn.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("ContactSearchByIDValue", sqlConn);
+                sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                sqlDa.SelectCommand.Parameters.AddWithValue("@SearhValue", textBox5.Text.Trim());
+                DataTable dbTbl = new DataTable();
+                sqlDa.Fill(dbTbl);
+                dataGridView1.DataSource = dbTbl;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
+            Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
+            worksheet = workbook.Sheets["Лист1"];
+            worksheet = workbook.ActiveSheet;
+            worksheet.Name = "Список";
+
+            for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+            {
+                worksheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+            }
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    worksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                }
+            }
+
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.FileName = "Contacts";
+            saveFileDialog.DefaultExt = ".xlsx";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                workbook.SaveAs(saveFileDialog.FileName, Type.Missing, Type.Missing, Type.Missing, 
+                    Type.Missing, Type.Missing,Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, 
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            }
+            app.Quit();
+        }
+    }
+}
